a68ebee69d5cefdb8e24d363bfcf441269221c3e
Madeline Guman <mads890@yahoo.com>
2020-05-15 08:43:38 -0400

    updated context, finished the note component

--- src/composition/FilesContext.js
+++ src/composition/FilesContext.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 const FilesContext = {
     folders: [],
     notes: [],
-    onDeleteNote: () => {}
+    addFolder: () => {},
+    addNote: () => {},
+    deleteNote: () => {},
 }
 
 export default FilesContext;
--- src/composition/Note.js
+++ src/composition/Note.js
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import FilesContext from './FilesContext';
 
-export default function Note(props) {
-    
+export default class Note extends Component {
+    static defaultProps = {
+        onDeleteNote: () => {},
+    }
+    static contextType = FilesContext;
+
+    handleDeleteNote = (e) => {
+        e.preventDefault;
+        const id = this.props.id
+        const url = `localhost:3000/notes/${id}`
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }
+        fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                //throw new Error(`Something went wrong: ${response.message}`) ?
+                return response.json()
+                .then(err => Promise.reject(err))
+            }
+            return response.json()
+        })
+        .then(() => {
+            this.context.deleteNote(id)
+            this.props.onDeleteNote(id)
+        })
+        .catch(err => {
+            console.error({ err })
+        })
+    }
+
+    render() {
+        const { title, id, date } = this.props
+        return (
+            <div className='note-list-item'>
+                <h2>
+                    <Link to={`/note/${id}`}>
+                        {title}
+                    </Link>
+                </h2>
+                <button 
+                    type='button'
+                    onClick={this.handleDeleteNote}
+                    className='delete-button'
+                >
+                    Delete
+                </button>
+                <div className='date'>
+                    <p>Date modified on <span>{date}</span></p>
+                </div>
+            </div>
+        );
+    }
 }
