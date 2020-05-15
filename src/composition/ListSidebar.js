7adf431a4bb8dee0a6c0841debe4da2bc6b7260b
Madeline Guman <mads890@yahoo.com>
2020-05-15 09:09:09 -0400

    finished sidebar components

--- src/composition/ListSidebar.js
+++ src/composition/ListSidebar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import FilesContext from './FilesContext';
 
 export default class ListSidebar extends Component {
+    static contextType = FilesContext;
+
+    getNumNotes = (notes, folderId) => {
+        notes.filter(note => note.folderId === folderId).length
+    }
 
     render() {
+        const { folders, notes } = this.context
         return(
-
+            <div className='list-sidebar sidebar'>
+                <ul className='folders-list'>
+                    {folders.map(folder => 
+                        <li key={folder.id}>
+                            <Link to={`/folder/${folder.id}`} className='folder-link'>
+                                <p><span>{this.getNumNotes(notes, folder.id)}</span> {folder.name}</p>
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+                <Link to='/add-folder' className='add-folder-link'>
+                    Add Folder
+                </Link>
+            </div>
         );
     }
 }
--- src/composition/NotePgSidebar.js
+++ src/composition/NotePgSidebar.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import FilesContext from './FilesContext';
 
 export default class NotePgSidebar extends Component {
+    static defaultProps = {
+        history: {
+            goBack: () => {}
+        },
+        match: {
+            params: {}
+        }
+    }
+    static contextType = FilesContext;
+
+    getNote = (noteList, id) => {
+        noteList.find(note => note.id === id)
+    }
+
+    getFolder = (folderList, id) => {
+        folderList.find(folder => folder.id === id)
+    }
 
     render() {
+        const { notes, folders } = this.context
+        const { id } = this.props.match.params
+        const note = this.getNote(notes, id)
+        const folder = this.getFolder(folders, note.folderId)
         return (
-
+            <div className='note-sidebar sidebar'>
+                <button
+                    type='button'
+                    value='Back'
+                    className='back-button'
+                    onClick={() => this.props.history.goBack()}
+                />
+                <h3>{folder.name}</h3>
+            </div>
         );
     }
 }
